39c483d07499b67e1819cf9e4198babfa3bc2c63
8an3 <44673979+8an3@users.noreply.github.com>
2023-05-23 01:59:14 -0400

    1

---
+++ netlify/functions/models.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const csv = require('csv-parser');
+const fs = require('fs');
+
+const router = express.Router();
+const PORT = 3000;
+
+router.get('/:model', (req, res) => {
+  const { model } = req.params;
+  const results = [];
+
+  fs.createReadStream('./data.csv')
+    .pipe(csv())
+    .on('data', (data) => {
+      results.push(data);
+    })
+    .on('end', () => {
+      const modelData = results.find((row) => row.model === model);
+      if (modelData) {
+        const { model1, msrp, freight, pdi, commodity, modelCode } = modelData;
+        res.json({ model1, msrp, freight, pdi, commodity, modelCode });
+      } else {
+        res.status(404).json({ error: `Model "${model}" not found` });
+      }
+    })
+    .on('error', (error) => {
+      console.error('Error:', error.message);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+module.exports = router;
--- netlify/functions/server-control.js
+++ netlify/functions/server-control.js
@@ -1,16 +1,49 @@
-// server-control.js
-
 // Required dependencies or libraries
 const { exec } = require('child_process');
 
 // Constants for paths and commands
-const DATA_PATH = './data.csv';
-const SERVER_START_COMMAND = 'node app.js';
-const SERVER_STOP_COMMAND = 'killall node';
+const BOATS_SERVER_PATH = '/api/apiBoats/app.js';
+const MODELS_SERVER_PATH = '/api/apiModels/app.js';
+const SERVER_START_COMMAND = 'node';
 
 // Function to start the server
-const startServer = () => {
-  exec(SERVER_START_COMMAND, (error, stdout, stderr) => {
+const startServer = (serverPath) => {
+  exec(`${SERVER_START_COMMAND} ${serverPath}`, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error starting server: ${error}`);
+      return;
+    }
+    console.log('Server started successfully!');
+  });
+};
+
+// Function to handle the HTTP request and start the server
+const handleStartServerRequest = async (event, context) => {
+  // Start the server logic
+  startServer(BOATS_SERVER_PATH);
+  startServer(MODELS_SERVER_PATH);
+
+  return {
+    statusCode: 200,
+    body: 'Server started successfully!',
+  };
+};
+
+// Export the serverless function
+exports.handler = handleStartServerRequest;
+
+
+/*// Required dependencies or libraries
+const { exec } = require('child_process');
+
+// Constants for paths and commands
+const BOATS_SERVER_PATH = '.netlify/api/apiBoats/app.js';
+const MODELS_SERVER_PATH = '.netlify/api/apiModels/app.js';
+const SERVER_START_COMMAND = 'node';
+
+// Function to start the server
+const startServer = (serverPath) => {
+  exec(`${SERVER_START_COMMAND} ${serverPath}`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Error starting server: ${error}`);
       return;
@@ -21,7 +54,7 @@ const startServer = () => {
 
 // Function to stop the server
 const stopServer = () => {
-  exec(SERVER_STOP_COMMAND, (error, stdout, stderr) => {
+  exec('killall node', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error stopping server: ${error}`);
       return;
@@ -30,34 +63,24 @@ const stopServer = () => {
   });
 };
 
-// Function to clear the data
-const clearData = () => {
-  // Implement logic to clear or truncate the data file (data.csv) here
-  console.log('Data cleared successfully!');
-};
-
-// Function to update the data
-const updateData = () => {
-  // Implement logic to update the data file (data.csv) here
-  console.log('Data updated successfully!');
-};
-
-// Example of timed starting and stopping
-const startAndStopTimed = () => {
-  // Start the server
-  startServer();
-
-  // Wait for a specific duration (e.g., 10 seconds) before stopping the server
-  setTimeout(() => {
+// Function to handle the command
+const handleCommand = (command) => {
+  if (command === 'hello') {
+    console.log('Hello, world!');
+    // Stop the servers after replying
     stopServer();
-  }, 10000); // 10 seconds
+  } else {
+    console.log('Unknown command!');
+  }
 };
 
-// Export the functions or endpoints for use as Netlify serverless functions
-module.exports = {
-  startServer,
-  stopServer,
-  clearData,
-  updateData,
-  startAndStopTimed,
-};
+// Start the boats server
+startServer(BOATS_SERVER_PATH);
+
+// Start the models server
+startServer(MODELS_SERVER_PATH);
+
+// Simulate receiving a command
+const command = 'hello'; // Change this to the desired command
+handleCommand(command);
+*/
